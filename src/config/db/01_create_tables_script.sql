639c56b045533c531a58db511f7119935c13e9de
jgalvez11 <jhegalvez11@gmail.com>
2023-11-20 04:36:14 -0500

    add script insertion

--- src/config/db/01_create_tables_script.sql
+++ src/config/db/01_create_tables_script.sql
@@ -222,22 +222,3 @@ ALTER TABLE "medical_order" ADD FOREIGN KEY ("appointment_id") REFERENCES "appoi
 ALTER TABLE "medical_order_medicine" ADD FOREIGN KEY ("order_id") REFERENCES "medical_order" ("order_id");
 
 ALTER TABLE "medical_order_medicine" ADD FOREIGN KEY ("medicine_id") REFERENCES "medicine" ("medicine_id");
-
-
-
-INSERT INTO "medicine" ("name", "description")
-VALUES
-  ('Paracetamol', 'Medicamento analgésico y antipirético comúnmente utilizado para tratar el dolor y reducir la fiebre.'),
-  ('Omeprazol', 'Medicamento inhibidor de la bomba de protones, usado para tratar úlceras gástricas y reflujo ácido.'),
-  ('Amoxicilina', 'Antibiótico de amplio espectro efectivo contra infecciones bacterianas.'),
-  ('Ibuprofeno', 'Analgésico y antiinflamatorio no esteroideo (AINE) para tratar dolor e inflamación.'),
-  ('Atorvastatina', 'Medicamento para reducir los niveles de colesterol en sangre.');
-
-INSERT INTO "specialty" ("name")
-VALUES
-  ('Cardiología'),
-  ('Pediatría'),
-  ('Dermatología'),
-  ('Gastroenterología'),
-  ('Neurología');
-
